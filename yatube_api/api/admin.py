c71be9d2f1800f2ba0f16609a8ca5549d027228e
Григорук Илья <friend2@ngs.ru>
2023-08-28 18:11:44 +0300

    Проект на ревью 1

--- yatube_api/api/admin.py
+++ yatube_api/api/admin.py
@@ -1,8 +1,3 @@
-from django.contrib import admin
-# from posts.models import User, Post, Group, Comment
+# from django.contrib import admin
 
 # # Register your models here.
-# admin.site.register(User)
-# admin.site.register(Post)
-# admin.site.register(Group)
-# admin.site.register(Comment)
